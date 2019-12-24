ee7b662d1fc64b8dea4ec14b7db934945ccceeb3
trroyel <royel.tanvir@gmail.com>
2019-12-24 16:27:13 +0600

    Added updateIncome support

--- src/store/reducers/incomes.js
+++ src/store/reducers/incomes.js
@@ -7,26 +7,39 @@ const initialState = {
 
 const incomeReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actions.FETCH_INCOMES:{
-            return{
-                ...state, 
+        case actions.FETCH_INCOMES: {
+            return {
+                ...state,
                 incomes: action.incomes,
                 updated: Date.now()
             };
         };
-
         case actions.ADD_INCOME: {
             return {
                 ...state,
                 incomes: state.incomes.concat(action.income)
             };
         };
-
         case actions.DELETE_INCOME: {
             return {
                 ...state,
-                incomes: state.incomes.filter(income=> income.id !== action.id)
+                incomes: state.incomes.filter(income => income.id !== action.id)
             };
+        };
+        case actions.UPDATE_INCOME: {
+            const incomes = [...state.incomes];
+            const index = incomes.findIndex(income => income.id === action.income.id);
+
+            if (index !== -1) {
+                incomes[index] = {
+                    ...incomes[index],
+                    ...action.income
+                };
+            }
+            return {
+                ...state,
+                incomes: incomes
+            }
         };
 
         default: return state;
